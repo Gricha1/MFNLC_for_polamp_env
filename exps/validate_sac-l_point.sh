ea627a5e1d50d42944f7ac9908c3a7c8dc7b29b9
Gricha1 <trololoeeev@mail.ru>
2025-09-02 17:13:38 +0300

    gregory: add exps configs

---
+++ exps/validate_sac-l_point.sh
@@ -0,0 +1,9 @@
+cd ../mfnlc
+python exps/train/obstacle/ris/point.py --train_sac\
+                                        --validate \
+                                        --total_timesteps 1 \
+                                        --validate_freq 1 \
+                                        --load_model --load_model_folder mhyn637i \
+                                        --add_subgoal_reinforce_sg_num 0 \
+                                        --validate_video_idx 4 \
+                                        #--validate_subgoal_video 
